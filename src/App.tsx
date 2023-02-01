8fa3e0e038d2e3d3edfb0b44bdbfc8f0d2a03769
Emilixn <emilien.muck@gmail.com>
2023-02-01 15:18:52 +0100

    Adding redux

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {useEffect} from "react";
 import InstalikeApi from "./instalikeApi";
 import './i18n'
 import {useTranslation} from "react-i18next";
+import Home from "./component/home";
 
 
 function App() {
@@ -20,7 +21,7 @@ function App() {
   return (
     <div className="App">
     <p>{t('actions.follow')}</p>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline ">
         Hello world!
       </h1>
     </div>
---
+++ src/component/card.tsx
@@ -0,0 +1,8 @@
+function card() {
+    return(<div className="card">
+
+        <div className="text-5xl">JE SUIS UNE CARD</div>
+    </div>)
+
+}
+export default card()
