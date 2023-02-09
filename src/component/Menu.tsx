efbf95ace32dd457e592388ac73167ad560d83f1
Emilixn <emilien.muck@gmail.com>
2023-02-09 17:24:31 +0100

    change link

--- src/component/Menu.tsx
+++ src/component/Menu.tsx
@@ -21,13 +21,13 @@ const Menu = () => {
                   className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                   aria-current="page"
                 >
-                  <Link to="feed">Home</Link>
+                  <Link to="/feed">Home</Link>
                 </div>
               </li>
 
               <div className="block py-2 pl-3 pr-4 text-gray-700 rounded md:p-0   ">
                 <li>
-                  <Link to="discover">Discover</Link>
+                  <Link to="/discover">Discover</Link>
                 </li>
               </div>
 
